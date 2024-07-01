831e2c21c2949ca40dfe0718d7b86d5236d71246
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-07-01 15:18:49 +0400

    Add module in charge of switching forms
    Allow the user to select a form to create a task,
    a project, or a note by clicking buttons.

---
+++ src/modules/switchForm.js
@@ -0,0 +1,23 @@
+const switchForm = (function () {
+
+    const formSections = document.querySelectorAll(".side-form p");
+    const taskForm = document.querySelector(".task-form");
+    const projectForm = document.querySelector(".project-form");
+    const noteForm = document.querySelector(".note-form");
+
+    formSections.forEach(section => {
+        section.addEventListener("click", () => {
+            switchForm(section.textContent.toLowerCase());
+        });
+    })
+
+    const switchForm = (formType) => {
+        taskForm.style.display = (formType === "task") ? "flex" : "none";
+        projectForm.style.display = (formType === "project") ? "flex" : "none";
+        noteForm.style.display = (formType === "note") ? "flex" : "none";
+    }
+
+    return {switchForm};
+})();
+
+export default switchForm;
