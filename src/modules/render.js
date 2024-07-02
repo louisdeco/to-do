0a6d4eb6186fb614a56ce22f72a48c79e56cc0ec
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-07-02 10:42:43 +0400

    Add module in charge of rendering tasks & projects
    Allow the content of the local storage to be disp-
    layed in the UI for the user.

---
+++ src/modules/render.js
@@ -0,0 +1,55 @@
+import stateActions from "./stateActions";
+
+const render = (function () {
+    const content = document.querySelector(".content");
+    const projectContainer = document.querySelector(".projects");
+
+    const createTaskElement = (task) => {
+        const taskDiv = document.createElement("div");
+        taskDiv.className = `task ${task.getPriority()}`;
+        taskDiv.innerHTML = `
+            <div class="left">
+                <input type="checkbox" name="check-task">
+                <div class="title">${task.getTitle()}</div>
+            </div>
+            <div class="right">
+                <button class="button-details">DETAILS</button>
+                <div class="date">${task.getDueDate()}</div>
+                <div class="edit"></div>
+                <div class="delete"></div>
+            </div>
+        `;
+        return taskDiv;
+    };
+
+    const createProjectElement = (project) => {
+        const projectParagraph = document.createElement("p");
+        projectParagraph.textContent = project.getName();
+        return projectParagraph;
+    };
+
+    const renderTasks = () => {
+        const tasks = stateActions.getTasks();
+        content.innerHTML = "";
+        tasks.forEach(task => {
+            const taskElement = createTaskElement(task);
+            content.appendChild(taskElement);
+        });
+    };
+
+    const renderProjects = () => {
+        const projects = stateActions.getProjects();
+        projectContainer.innerHTML="";
+        projects.forEach(project => {
+            const projectElement = createProjectElement(project);
+            projectContainer.appendChild(projectElement);
+        });
+    };
+
+    return {
+        renderTasks,
+        renderProjects
+    }
+})();
+
+export default render;
