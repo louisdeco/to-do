c5ce9007bad03dc8034e0523f4f93ee2eb034e78
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-07-19 11:15:56 +0400

    Add module in charge of handling the forms
    Allow the user to add tasks and projects through
    the UI.

---
+++ src/modules/formHandler.js
@@ -0,0 +1,48 @@
+import stateActions from "./stateActions";
+import render from "./render";
+import switchForm from "./switchForm";
+import { set } from "date-fns/fp/set";
+
+const formHandler = (function () {
+    const form = document.querySelector(".add-element")
+    const addButton = document.querySelector(".add");
+    const dialog = document.querySelector(".dialog-form")
+
+    const handleFormSubmission = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const taskForm = e.target.querySelector(".task-form");
+        const projectForm = e.target.querySelector(".project-form");
+
+        if (taskForm && taskForm.style.display !== "none") {
+            const title = formData.get("task-title");
+            const description = formData.get("task-details");
+            const dueDate = formData.get("task-date");
+            const priority = formData.get("task-priority");
+
+            stateActions.addTask(title, description, dueDate, priority);
+            render.renderTasks();
+            dialog.close();
+        }
+
+        else if (projectForm && projectForm.style.display !== "none"){
+            const title = formData.get("project-title");
+
+            stateActions.addProject(title);
+            render.renderProjects();
+            dialog.close()       }
+    }
+
+    const init = () => {
+        form.addEventListener("submit", handleFormSubmission);
+        addButton.addEventListener("click", () => {
+            switchForm.init();
+            form.reset();
+            dialog.showModal();
+        });
+    }
+
+    return {init}
+})();
+
+export default formHandler;
