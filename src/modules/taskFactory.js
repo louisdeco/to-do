a3bf661f6a57c2ef86921029cb12aa1fba129f57
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-08-15 15:18:53 +0400

    Add current project to the creation of tasks
    Allow each task to have the project they are assi-
    gned to.

--- src/modules/taskFactory.js
+++ src/modules/taskFactory.js
@@ -1,11 +1,14 @@
 import taskModel from "./taskModel";
+import tabInteractivity from "./tabInteractivity";
 import { v4 as uuidv4 } from "uuid";
 
 const taskFactory = (function () {
 
     const createTask = (title, description, dueDate, priority, isDone = false) => {
         const taskId = uuidv4();
-        const task = taskModel(title, description, dueDate, priority, taskId, isDone);
+        const currentProject = tabInteractivity.getCurrentTab();
+        const task = taskModel(title, description, dueDate, priority, taskId, currentProject, isDone);
+
        return task;
     }
     
