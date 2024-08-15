6be7a194d94d480f2ef69f1d48a8f458264f4409
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-08-15 15:09:48 +0400

    Add project variable to the task element
    Allow to link a given project to the task object.

--- src/modules/taskModel.js
+++ src/modules/taskModel.js
@@ -1,9 +1,10 @@
-function taskModel(title, description, dueDate, priority, id, isDone = false) {
+function taskModel(title, description, dueDate, priority, id, project, isDone = false) {
     let _title = title;
     let _description = description;
     let _dueDate = dueDate;
     let _priority = priority;
     let _id = id;
+    let _project = project;
     let _isDone = isDone;
 
     const getTitle = () => _title;
@@ -21,9 +22,11 @@ function taskModel(title, description, dueDate, priority, id, isDone = false) {
     const getId = () => _id;
 
     const getIsDone = () => _isDone;
-    const setIsDone = (newIsDone) => {_isDone = newIsDone}
+    const setIsDone = (newIsDone) => {_isDone = newIsDone};
 
-    return {getTitle, setTitle, getDescription, setDescription, getDueDate, setDueDate, getPriority, setPriority, getIsDone, getId, setIsDone};
+    const getProject = () => _project;
+
+    return {getTitle, setTitle, getDescription, setDescription, getDueDate, setDueDate, getPriority, setPriority, getIsDone, getId, setIsDone, getProject};
 }
 
 export default taskModel;
