af64746c6af4eadd3d51245ddc48f88e85285ba1
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-08-15 15:20:59 +0400

    Add functions related to assigning the tabs.
    Addition of the initDefaultTab function in charge
    of setting the current tab as "Today" by default.
    
    Addition of the selectCurrentTab function in char-
    ge of selecting the current tab.

--- src/modules/tabInteractivity.js
+++ src/modules/tabInteractivity.js
@@ -2,12 +2,20 @@ import render from "./render";
 import stateActions from "./stateActions";
 
 const tabInteractivity = (function () {
+    let _currentTab = null;
 
     const init = () => {
         const projects = document.querySelectorAll(".project-element");
+        const tabs = document.querySelectorAll(".navigation");
+
         projects.forEach(project => {
 
             deleteProject(project);
+        })
+
+        tabs.forEach(tabElement => {
+
+            selectCurrentTab(tabs, tabElement);
         })
     }
 
@@ -18,10 +26,30 @@ const tabInteractivity = (function () {
         projectButton.addEventListener("click", () => {
             stateActions.deleteProject(projectId);
             render.renderProjects();
-        })
-    }
+        });
+    };
 
-    return {init};
+    const selectCurrentTab = (tabs, tabElement) => {
+        const tabName = tabElement.textContent;
+        tabElement.addEventListener("click", () => {
+            setCurrentTab(tabName);
+            tabs.forEach(tab => tab.classList.remove("current-tab"));
+            tabElement.classList.add("current-tab");
+        });
+    };
+
+    const initDefaultTab = () => {
+        const defaultTab = document.querySelector(".default");
+        const tabName = defaultTab.textContent;
+        setCurrentTab(tabName);
+        defaultTab.classList.add("current-tab");
+    };
+
+    const getCurrentTab = () => _currentTab;
+
+    const setCurrentTab = (newTab) => {_currentTab = newTab};
+
+    return {init, getCurrentTab, initDefaultTab};
 
 })();
 
